e3756e7b0f7e805db15330164ae1aa84d713625e
artem <artem.marshev@kodep.ru>
2022-10-06 17:19:16 +0300

    add based nextjs app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCcYqEfUiaGP3LkCNzaoFbUNjwLKXBYXac",
+  authDomain: "netflix-clone-marshev.firebaseapp.com",
+  projectId: "netflix-clone-marshev",
+  storageBucket: "netflix-clone-marshev.appspot.com",
+  messagingSenderId: "251041636950",
+  appId: "1:251041636950:web:3c87f8b012c54c8060d1f7",
+};
